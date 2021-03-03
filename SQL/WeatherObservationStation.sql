81a54c43b21925f50e93cac7416db2330ad9e26c
teospeece <59575384+teospeece@users.noreply.github.com>
2021-03-03 13:22:51 -0600

    Update WeatherObservationStation.sql

--- SQL/WeatherObservationStation.sql
+++ SQL/WeatherObservationStation.sql
@@ -1,8 +1,7 @@
 /*
 Teo SPeece
 Weather Observation Station 3
-This was the 10th challenge which completed the basic select
-challenges on HackerWars
+One of challenges on Basic Select onHackerWars
 Instructions: Query a list of CITY names from STATION for 
 cities that have an even ID number. Print the results in any 
 order, but exclude duplicates from the answer.
